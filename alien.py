858c814022c1b846a2ad7feb9529b76fb69489ca
codeagles <hcn0521@outlook.com>
2017-09-21 22:03:16 +0800

    优化代码

--- alien.py
+++ alien.py
@@ -6,6 +6,9 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+
+
+
 def run_game():
 
     # 初始化游戏并创建一个屏幕对象
@@ -18,7 +21,7 @@ def run_game():
     ship = Ship(ai_sttings, screen)
     # 创建一个用于存储子弹的编组
     bullets = Group()
-    #开始游戏的主循环
+    # 开始游戏的主循环
     while True:
         gf.check_events(ai_sttings, screen,ship,bullets)
         ship.update()
