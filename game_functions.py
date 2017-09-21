35ac0a733f5fc85f49519dd753ac3bc5e8306552
codeagles <hcn0521@outlook.com>
2017-09-21 22:02:57 +0800

    优化代码

--- game_functions.py
+++ game_functions.py
@@ -18,7 +18,6 @@ def check_events(ai_settings, screen, ship, bullets):
 
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
-
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
@@ -29,7 +28,6 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
 
 
 def fire_bullet(ai_settings, screen, ship, bullets):
-
     """如果还没有到达限制，就发射一颗子弹"""
     # 创建新子弹
     if len(bullets) < ai_settings.bullets_allowed:
@@ -38,7 +36,6 @@ def fire_bullet(ai_settings, screen, ship, bullets):
 
 
 def check_keyup_events(event, ship):
-
     """响应松开"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
@@ -47,7 +44,6 @@ def check_keyup_events(event, ship):
 
 
 def update_screen(ai_settings, screen, ship, bullets):
-
     """更新屏幕上的图像，并切换到新屏幕"""
     # 每次循环时都重新绘制屏幕
     screen.fill(ai_settings.bg_color)
@@ -58,6 +54,7 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
     # 让最近绘制的屏幕可见
     pygame.display.flip()
+
 
 def update_bullets(bullets):
     """更新子弹的位置，并删除已消失的子弹"""
